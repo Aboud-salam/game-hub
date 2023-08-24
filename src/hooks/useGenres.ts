eff5dd401020b51868c20df385e3d8023f00a7a9
Aboud A-Salam <ar.48.salam@gmail.com>
2023-08-25 00:57:43 +0300

    Filtering games by genres using it's id as a queryString param

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -4,5 +4,5 @@ export interface Genre {
   name: string;
   image_background: string;
 }
-const useGenres = () => useData<Genre>("/genres");
+const useGenres = () => useData<Genre>("/genres", {});
 export default useGenres;
