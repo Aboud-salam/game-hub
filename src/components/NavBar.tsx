bac5f928b6be9dc446fc2019d211cf249b63174e
Aboud A-Salam <ar.48.salam@gmail.com>
2023-08-26 15:43:16 +0300

    Implemented the search logic

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,12 +2,14 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import { ColorModeSwitch } from "./ColorModeSwitch";
 import SearchingBox from "./SearchingBox";
-
-export const NavBar = () => {
+interface Props {
+  onSearch: (searcText: string) => void;
+}
+export const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack gap={4} padding="10px">
       <Image src={logo} boxSize={"60px"} />
-      <SearchingBox />
+      <SearchingBox onSearch={(searcText) => onSearch(searcText)} />
       <ColorModeSwitch />
     </HStack>
   );
--- src/components/SearchingBox.tsx
+++ src/components/SearchingBox.tsx
@@ -1,16 +1,29 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
-const SearchingBox = () => {
+interface Props {
+  onSearch: (searcText: string) => void;
+}
+const SearchingBox = ({ onSearch }: Props) => {
+  const searchInput = useRef<HTMLInputElement>(null);
   return (
-    <InputGroup>
-      <InputLeftElement children={<BsSearch />} />
-      <Input
-        borderRadius={20}
-        variant={"filled"}
-        type="text"
-        placeholder="Search games..."
-      />
-    </InputGroup>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (searchInput.current) onSearch(searchInput.current.value);
+      }}
+    >
+      <InputGroup>
+        <InputLeftElement children={<BsSearch />} />
+        <Input
+          ref={searchInput}
+          borderRadius={20}
+          variant={"filled"}
+          type="text"
+          placeholder="Search games..."
+        />
+      </InputGroup>
+    </form>
   );
 };
 
