90ae84b6b3b2138b698a45727f7b508554e7fa1b
Aboud A-Salam <ar.48.salam@gmail.com>
2023-08-26 03:10:30 +0300

    Handled games with no image

--- src/services/imgae-url.ts
+++ src/services/imgae-url.ts
@@ -1,5 +1,6 @@
+import noImage from "../assets/no-image-placeholder-6f3882e0.webp";
 const getCroppedImgUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImage;
   let target = "media/";
   let targetIndex = url.indexOf(target) + target.length - 1;
   let modImgUrl = url
