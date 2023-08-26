7e10cb8ac8a62a4c5bb8cb246a9df0e25b21688a
Aboud A-Salam <ar.48.salam@gmail.com>
2023-08-26 17:14:18 +0300

    Refactor: using context hook

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,14 +2,11 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import { ColorModeSwitch } from "./ColorModeSwitch";
 import SearchingBox from "./SearchingBox";
-interface Props {
-  onSearch: (searcText: string) => void;
-}
-export const NavBar = ({ onSearch }: Props) => {
+export const NavBar = () => {
   return (
     <HStack gap={4} padding="10px">
       <Image src={logo} boxSize={"60px"} />
-      <SearchingBox onSearch={(searcText) => onSearch(searcText)} />
+      <SearchingBox />
       <ColorModeSwitch />
     </HStack>
   );
